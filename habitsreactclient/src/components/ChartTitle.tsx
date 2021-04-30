56a18edcbb50ab58603103675763dfaa4c03130e
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-04-30 20:16:55 -0300

    Removed typing animation from chartitle component

--- habitsreactclient/src/components/ChartTitle.tsx
+++ habitsreactclient/src/components/ChartTitle.tsx
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Text } from "@chakra-ui/react";
 import { TypingAnimation } from "./AnimatedChakraComponents";
 export class ChartTitle extends React.Component<any, any> {
   render() {
     return (
-      <VStack w="80px">
-        <TypingAnimation
-          isTruncated={true}
-          style={{ border: "none" }}
-          textAlign="center"
-          writeOnly={true}
-          durationInMS={500}
-          fontWeight="bold"
-          onMountOnly={true}
-          fontFamily="serif"
-          fontSize={{ base: "12px", md: "14px" }}
-          text={"ASD "}
-        />
-      </VStack>
+      <Text
+        isTruncated={true}
+        style={{ border: "none" }}
+        textAlign="center"
+        fontWeight="bold"
+        fontFamily="serif"
+        fontSize={{ base: "12px", md: "14px" }}
+      >
+        {this.props.text}
+      </Text>
     );
   }
 }
