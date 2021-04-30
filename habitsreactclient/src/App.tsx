869b71bf34d11990e00d987817cb35d5ecbf5969
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-04-30 20:21:48 -0300

    Added a quick fix to avoid double rendering components after user signs in, removed componentDidUpdate method and added Router component

--- habitsreactclient/src/App.tsx
+++ habitsreactclient/src/App.tsx
@@ -12,6 +12,7 @@ import AppTitleContainer from "./components/AppTitleContainer";
 import AuthenticationPanelContainer from "./components/AuthenticationPanelContainer";
 import UserServicesContainer from "./components/UserServicesContainer";
 import { AppMenu } from "./components/AppMenu";
+import { BrowserRouter as Router } from "react-router-dom";
 interface AppState {
   headerTitle: string;
   authenticated: boolean;
@@ -20,9 +21,163 @@ interface AppState {
   userTitle: string;
   appTitle: string;
   secondAppSectionWidth: string;
+  horribleDoubleRenderSolution: boolean;
 }
 interface AppProps {}
 
+export class App extends React.Component<AppProps, AppState> {
+  // https://github.com/chakra-ui/chakra-ui/issues/591
+  constructor(props: any) {
+    super(props);
+    console.log("constructed");
+    this.setUserCredentials = this.setUserCredentials.bind(this);
+    this.logout = this.logout.bind(this);
+    const isUserAuthenticated: boolean = localStorage.getItem("token") !== null;
+    let userTitle: string = "";
+    if (isUserAuthenticated) {
+      let storedUserNameValue: string = localStorage.getItem(
+        "userName"
+      ) as string;
+      userTitle =
+        storedUserNameValue[storedUserNameValue.length - 1] === "s"
+          ? storedUserNameValue + "' "
+          : storedUserNameValue + "'s ";
+    }
+    this.state = {
+      secondAppSectionWidth: isUserAuthenticated ? "100%" : "50%",
+      bigScreensAppOrientation: isUserAuthenticated ? "column" : "row",
+      headerTitle: "HaBits ~ Track & Trace",
+      appTitle: "HaBits",
+      authenticated: isUserAuthenticated,
+      userTitle: userTitle,
+      horribleDoubleRenderSolution: isUserAuthenticated,
+    };
+  }
+  URL: string = "http://127.0.0.1:8000";
+  setUserCredentials(layOutChangeSpeed: number) {
+    const userURL: string = this.URL + "/user/";
+    axios
+      .get(userURL, {
+        headers: { Authorization: "Token " + localStorage.getItem("token") },
+      })
+      .then((response) => {
+        const userInfo: any = response.data.results[0];
+        localStorage.setItem("userName", userInfo.username);
+        localStorage.setItem("userId", userInfo.id);
+        let userTitle: string =
+          userInfo.username[userInfo.username.length - 1] === "s"
+            ? userInfo.username + "' "
+            : userInfo.username + "'s ";
+        let updatedState: any = {
+          authenticated: true,
+          userTitle: userTitle,
+          titleSubText: "",
+          bigScreensAppOrientation: "row",
+          horribleDoubleRenderSolution: false,
+        };
+        this.setState(updatedState);
+        // App Orientation for non-mobile/tablets screens changes only after
+        // login or logout animations are finished.
+        setTimeout(() => {
+          this.setState({
+            bigScreensAppOrientation: "column",
+            secondAppSectionWidth: "100%",
+            horribleDoubleRenderSolution: true,
+          });
+        }, layOutChangeSpeed);
+      })
+      .catch((error) => console.log(error));
+  }
+  logout(e: any) {
+    e.preventDefault();
+    for (let information of ["token", "userName", "id"]) {
+      localStorage.removeItem(information);
+    }
+    this.setState({
+      userTitle: "",
+      authenticated: false,
+      bigScreensAppOrientation: "row",
+      secondAppSectionWidth: "50%",
+    });
+  }
+  render() {
+    return (
+      <HelmetProvider>
+        <ChakraProvider
+          theme={extendTheme({
+            styles: {
+              global: (props: any) => ({
+                body: {
+                  bg: mode("#fcf6ef", "gray.800")(props),
+                },
+              }),
+            },
+          })}
+        >
+          <Router>
+            <Route path="/">
+              <Helmet title={this.state.headerTitle} />
+              <AppMenu
+                displayAsUserMenu={this.state.authenticated}
+                logOutUser={this.logout}
+              />
+              <Flex
+                direction={{
+                  base: "column",
+                  md: this.state.bigScreensAppOrientation,
+                }}
+                justify={{ base: "stretch", md: "" }}
+                align={{ base: "center", md: "" }}
+                h="100vh"
+              >
+                <AppTitleContainer
+                  displayAsUserTitle={this.state.authenticated}
+                >
+                  <AppTitle
+                    userTitle={this.state.userTitle}
+                    titleOnly={this.state.authenticated}
+                  />
+                </AppTitleContainer>
+                <Box
+                  w={{
+                    base: "100%",
+                    md: this.state.secondAppSectionWidth,
+                  }}
+                  h={{ base: "75%", md: "100%" }}
+                >
+                  <AnimatePresence>
+                    {!this.state.authenticated && (
+                      <>
+                        <AuthenticationPanelContainer>
+                          <AuthenticationPanel
+                            setUserCredentials={this.setUserCredentials}
+                          />
+                        </AuthenticationPanelContainer>
+                        <Redirect to="/login" />
+                      </>
+                    )}
+                  </AnimatePresence>
+                  {this.state.authenticated && (
+                    <>
+                      <UserServicesContainer>
+                        <UserServices
+                          hasRenderedTwice={
+                            this.state.horribleDoubleRenderSolution
+                          }
+                        />
+                      </UserServicesContainer>
+                    </>
+                  )}
+                </Box>
+              </Flex>
+            </Route>
+          </Router>
+        </ChakraProvider>
+      </HelmetProvider>
+    );
+  }
+}
+/*
 export class App extends React.Component<AppProps, AppState> {
   // https://github.com/chakra-ui/chakra-ui/issues/591
   constructor(props: any) {
@@ -47,10 +202,10 @@ export class App extends React.Component<AppProps, AppState> {
       appTitle: "HaBits",
       authenticated: isUserAuthenticated,
       userTitle: userTitle,
+      horribleDoubleRenderSolution: isUserAuthenticated,
     };
   }
   URL: string = "http://127.0.0.1:8000";
-  componentDidUpdate(_prevProps: any, _prevState: any) {}
   setUserCredentials(layOutChangeSpeed: number) {
     const userURL: string = this.URL + "/user/";
     axios
@@ -70,6 +225,7 @@ export class App extends React.Component<AppProps, AppState> {
           userTitle: userTitle,
           titleSubText: "",
           bigScreensAppOrientation: "row",
+          horribleDoubleRenderSolution: false,
         };
         this.setState(updatedState);
         // App Orientation for non-mobile/tablets screens changes only after
@@ -78,6 +234,7 @@ export class App extends React.Component<AppProps, AppState> {
           this.setState({
             bigScreensAppOrientation: "column",
             secondAppSectionWidth: "100%",
+            horribleDoubleRenderSolution: true,
           });
         }, layOutChangeSpeed);
       })
@@ -152,7 +309,11 @@ export class App extends React.Component<AppProps, AppState> {
                 {this.state.authenticated && (
                   <>
                     <UserServicesContainer>
-                      <UserServices />
+                      <UserServices
+                        hasRenderedTwice={
+                          this.state.horribleDoubleRenderSolution
+                        }
+                      />
                     </UserServicesContainer>
                   </>
                 )}
@@ -164,3 +325,4 @@ export class App extends React.Component<AppProps, AppState> {
     );
   }
 }
+ */
