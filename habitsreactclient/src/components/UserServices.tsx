0aef9a605e6dd21c1b3eac6a3877c4e4f9f3f575
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-05-16 20:22:24 -0300

    reloadCalendar functionality after new habit is created was moved to the app component. Added colorMode changing functionality to pass as props to component children

--- habitsreactclient/src/components/UserServices.tsx
+++ habitsreactclient/src/components/UserServices.tsx
@@ -1,24 +1,37 @@
 import { HabitCalendarInterface } from "./habitCalendar/HabitCalendarInterface";
 import { HabitsInterface } from "./habitsInterface/HabitsInterface";
 import * as React from "react";
-import { Flex } from "@chakra-ui/react";
-export class UserServices extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = { reloadCalendar: false };
-    this.setRealoadCalendar = this.setRealoadCalendar.bind(this);
-  }
-  setRealoadCalendar() {
-    this.setState((currentState: any, _currentProps: any) => {
-      return { reloadCalendar: !currentState.reloadCalendar };
-    });
-  }
-  render() {
-    return (
-      <Flex width="100%" h="100%" direction="column" align="center">
-        <HabitCalendarInterface key={String(this.props.calendarReloadKey)} />
-        <HabitsInterface />
-      </Flex>
-    );
-  }
+import { Flex, useColorModeValue } from "@chakra-ui/react";
+export function UserServices(props: any) {
+  const [userHabitData, setUserHabitData] = React.useState(undefined);
+  const lightColors: any = {
+    bgColor: "#fff",
+    text: "#333",
+    gradient: { c1: "#7928CA", c2: "#FF0080" },
+  };
+  const darkColors: any = {
+    bgColor: "#333",
+    text: "#fff",
+    gradient: { c1: "#788389", c2: "#A77" },
+  };
+  let colorMode: any = useColorModeValue(lightColors, darkColors);
+  let gradient: string =
+    "linear(to-r," + colorMode.gradient.c1 + "," + colorMode.gradient.c2 + ")";
+  let altGradient: string =
+    "linear(to-r," + colorMode.gradient.c2 + "," + colorMode.gradient.c1 + ")";
+  let textColor: any = colorMode.text;
+  let bgColor: any = colorMode.bgColor;
+
+  return (
+    <Flex width="100%" h="100%" direction="column" align="center">
+      <HabitCalendarInterface
+        liftUserHabitDataUp={setUserHabitData}
+        key={String(props.calendarReloadKey)}
+      />
+      <HabitsInterface
+        themeProps={{ gradient, altGradient, textColor, bgColor }}
+        userHabitData={userHabitData}
+      />
+    </Flex>
+  );
 }
