251bdd3d25d72d54f123292d35af41cef53d3f19
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-05-16 20:20:51 -0300

    Request network for user habit data deleted and this information is now passed as a prop

--- habitsreactclient/src/components/habitsInterface/HabitsInterface.tsx
+++ habitsreactclient/src/components/habitsInterface/HabitsInterface.tsx
@@ -1,50 +1,16 @@
 import * as React from "react";
 import { Flex } from "@chakra-ui/react";
-import axios from "axios";
 import { Habits } from "./Habits";
 
 export class HabitsInterface extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      habits: [],
-    };
-  }
-  async componentDidMount() {
-    try {
-      const habits: any = await this.getUserHabitList();
-      const state: any = { habits: habits };
-      this.setState(state);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async getUserHabitList() {
-    try {
-      const response: any = await axios.get("http://127.0.0.1:8000/habit", {
-        headers: { Authorization: "Token " + localStorage.getItem("token") },
-      });
-      let habits: any = response.data.results;
-      return habits;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   render() {
     return (
       <Flex h="75%" w="100%" align="center" justify="center">
-        <Flex
-          position="relative"
-          boxShadow="lg"
-          bgColor="white"
-          rounded="md"
-          h="90%"
-          w="90%"
-          direction="column"
-        >
-          <Habits data={this.state.habits} />
+        <Flex position="relative" h="90%" w="90%" direction="column">
+          <Habits
+            themeProps={this.props.themeProps}
+            data={this.props.userHabitData}
+          />
         </Flex>
       </Flex>
     );
