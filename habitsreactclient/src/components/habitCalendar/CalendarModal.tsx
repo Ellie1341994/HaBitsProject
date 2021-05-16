9c9b43d97338487bee0bd60cbad39a593030749b
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-05-16 15:56:22 -0300

    Icons representing user humor are now dynamically generated

--- habitsreactclient/src/components/habitCalendar/CalendarModal.tsx
+++ habitsreactclient/src/components/habitCalendar/CalendarModal.tsx
@@ -12,38 +12,53 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/react";
 import { FaFrownOpen, FaGrinBeam, FaGrinTongue } from "react-icons/fa";
+type cx = "c1" | "c2" | "c3";
+function makeUserHumorIcons(
+  iconColors: { c1: string; c2: string; c3: string },
+  humorValue: number | string
+) {
+  const icons: any[] = [];
+  // Object shorthand notation below
+  const iconComponents: any = { FaFrownOpen, FaGrinTongue, FaGrinBeam };
+  let index: number = 1;
+  for (let iconName in iconComponents) {
+    let iconMatchesValue: boolean = humorValue === index;
+    let ComponentName: any = iconComponents[iconName];
+    let colorType: cx = ("c" + index) as cx;
+    const Icon: any = (
+      <ComponentName
+        key={iconName}
+        size={"24"}
+        opacity={iconMatchesValue ? 1 : 0.5}
+        color={iconMatchesValue ? iconColors[colorType] : "#888"}
+      />
+    );
+    index++;
+    icons.push(Icon);
+  }
+  return icons;
+}
 export function CalendarModal(_props: any) {
   const lightColors: any = {
     bgColor: "#fff",
     text: "#333",
-    gradient: { c1: "#7928CA", c2: "#FF0080", c3: "#bc14a5" },
+    gradient: { c1: "#7928CA", c3: "#FF0080", c2: "#bc14a5" },
   };
   const darkColors: any = {
     bgColor: "#333",
     text: "#fff",
-    gradient: { c1: "#788389", c2: "#A77", c3: "#927d80" },
+    gradient: { c1: "#788389", c3: "#A77", c2: "#927d80" },
   };
   let colorMode: any = useColorModeValue(lightColors, darkColors);
   let gradient: string =
-    "linear(to-r," + colorMode.gradient.c1 + "," + colorMode.gradient.c2 + ")";
+    "linear(to-r," + colorMode.gradient.c1 + "," + colorMode.gradient.c3 + ")";
   let textColor: any = colorMode.text;
   let bgColor: any = colorMode.bgColor;
   let trackDate: string = _props.trackInformation?.dateCreated.substr(
     0,
     _props.trackInformation.dateCreated.indexOf("T")
   );
-  function setIconOpacity(trackEffectiveness: string, iconType: string) {
-    const effectivenessNumber: number = parseInt(trackEffectiveness);
-    if (
-      (iconType === "bad" && effectivenessNumber === 1) ||
-      (iconType === "medium" && effectivenessNumber === 2) ||
-      (iconType === "good" && effectivenessNumber === 3)
-    ) {
-      return 1;
-    }
 
-    return 0.5;
-  }
   return (
     <Modal isOpen={_props.open} onClose={_props.setOpen}>
       <ModalOverlay />
@@ -58,33 +73,10 @@ export function CalendarModal(_props: any) {
             <Text fontFamily="serif" fontSize={{ base: "md", md: "lg" }}>
               Habit Daily Review
             </Text>
-            <FaFrownOpen
-              size={"24"}
-              opacity={_props.trackInformation?.effectiveness === 1 ? 1 : 0.5}
-              color={
-                _props.trackInformation?.effectiveness === 1
-                  ? colorMode.gradient.c1
-                  : "black"
-              }
-            />
-            <FaGrinTongue
-              size={"24"}
-              opacity={_props.trackInformation?.effectiveness === 2 ? 1 : 0.5}
-              color={
-                _props.trackInformation?.effectiveness === 2
-                  ? colorMode.gradient.c3
-                  : "black"
-              }
-            />
-            <FaGrinBeam
-              size={"24"}
-              opacity={_props.trackInformation?.effectiveness === 3 ? 1 : 0.5}
-              color={
-                _props.trackInformation?.effectiveness === 3
-                  ? colorMode.gradient.c2
-                  : "black"
-              }
-            />
+            {makeUserHumorIcons(
+              colorMode.gradient,
+              _props.trackInformation?.effectiveness
+            )}
             <br />
           </Flex>
         </ModalHeader>
