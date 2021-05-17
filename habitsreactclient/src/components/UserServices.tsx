3864e384b03d9578761726b4880108e3556abe3b
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-05-17 15:31:56 -0300

    Added non-opaque color gradient to UserServices component

--- habitsreactclient/src/components/UserServices.tsx
+++ habitsreactclient/src/components/UserServices.tsx
@@ -7,16 +7,24 @@ export function UserServices(props: any) {
   const lightColors: any = {
     bgColor: "#fff",
     text: "#333",
-    gradient: { c1: "#7928CA", c2: "#FF0080" },
+    gradient: { c1: "#7928CA70", c2: "#FF008070" },
+    gradientText: { c1: "#7928CA", c2: "#FF0080" },
   };
   const darkColors: any = {
     bgColor: "#333",
     text: "#fff",
-    gradient: { c1: "#788389", c2: "#A77" },
+    gradient: { c1: "#78838970", c2: "#A777" },
+    gradientText: { c1: "#788389", c2: "#A77" },
   };
   let colorMode: any = useColorModeValue(lightColors, darkColors);
   let gradient: string =
     "linear(to-r," + colorMode.gradient.c1 + "," + colorMode.gradient.c2 + ")";
+  let gradientText: string =
+    "linear(to-r," +
+    colorMode.gradientText.c1 +
+    "," +
+    colorMode.gradientText.c2 +
+    ")";
   let altGradient: string =
     "linear(to-r," + colorMode.gradient.c2 + "," + colorMode.gradient.c1 + ")";
   let textColor: any = colorMode.text;
@@ -29,7 +37,13 @@ export function UserServices(props: any) {
         key={String(props.calendarReloadKey)}
       />
       <HabitsInterface
-        themeProps={{ gradient, altGradient, textColor, bgColor }}
+        themeProps={{
+          gradient,
+          gradientText,
+          altGradient,
+          textColor,
+          bgColor,
+        }}
         userHabitData={userHabitData}
       />
     </Flex>
