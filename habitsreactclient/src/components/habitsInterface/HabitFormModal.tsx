b7ca06d221e633bd2de19d58bf0010a598a9f4d2
Ellie1341994 <eli13_4_1994@hotmail.com>
2021-05-18 23:35:21 -0300

    Added a Review version of this component. It renders humor icons and a text box to add in a form to post request a habit track

--- habitsreactclient/src/components/habitsInterface/HabitFormModal.tsx
+++ habitsreactclient/src/components/habitsInterface/HabitFormModal.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import axios from "axios";
+import { FaFrownOpen, FaGrinBeam, FaGrinTongue } from "react-icons/fa";
 import {
+  Link,
   useColorModeValue,
   Textarea,
   Flex,
@@ -18,6 +20,71 @@ import {
   ModalBody,
   ModalCloseButton,
 } from "@chakra-ui/react";
+function EffectivenessIcons(props: any) {
+  const [iconStyle, setIconStyle] = React.useState({
+    effectiveness: -1,
+    opacity: 0.5,
+    color: ["#888", "#888", "#888"],
+  });
+  const lightColors: any = {
+    bgColor: "#fff",
+    text: "#333",
+    gradient: ["#7928CA", "#bc14a5", "#FF0080"],
+  };
+  const darkColors: any = {
+    bgColor: "#333",
+    text: "#fff",
+    gradient: ["#788389", "#927d80", "#A77"],
+  };
+  let colorMode: any = useColorModeValue(lightColors, darkColors);
+  const icons: any[] = [];
+  // Object shorthand notation below
+  const iconComponents: any = { FaFrownOpen, FaGrinTongue, FaGrinBeam };
+  let index: number = 1;
+  for (let iconName in iconComponents) {
+    let ComponentName: any = iconComponents[iconName];
+    let indexSnapshot: number = index - 1;
+    //colorMode.gradient[colorType]
+    const Icon: any = (
+      <Link
+        key={"sada" + indexSnapshot}
+        onClick={(event: any) => {
+          let newIconColor: any = iconStyle.color.map((_i: any) => "#8885");
+          newIconColor[indexSnapshot] = colorMode.gradient[indexSnapshot];
+          event.preventDefault();
+          setIconStyle({
+            effectiveness: indexSnapshot + 1,
+            opacity: 1,
+            color: newIconColor,
+          });
+        }}
+      >
+        <ComponentName
+          key={iconName}
+          size={"24"}
+          opacity={iconStyle.opacity}
+          color={iconStyle.color[indexSnapshot]}
+        />
+      </Link>
+    );
+    index++;
+    icons.push(Icon);
+  }
+  return (
+    <>
+      <Input
+        hidden={true}
+        name="trackEffectiveness"
+        id="trackEffectiveness"
+        value={iconStyle.effectiveness}
+        readOnly={true}
+      />
+      <Flex p="1" m="1" justify="space-evenly">
+        {icons}
+      </Flex>
+    </>
+  );
+}
 function makeSelectionOf(list: any, label: string, namePrefix: string) {
   const container: any = [];
   let extraElement: any = (
@@ -63,6 +130,18 @@ export function HabitFormModal(_props: any) {
     "linear(to-r," + colorMode.gradient.c1 + "," + colorMode.gradient.c2 + ")";
   let textColor: any = colorMode.text;
   let bgColor: any = colorMode.bgColor;
+  let modalTitle: string =
+    _props.type === "Edit"
+      ? "Edit " + _props.name
+      : _props.type === "Review"
+      ? _props.name + " Review"
+      : "Habit";
+  let buttonText: string =
+    _props.type === "Edit"
+      ? "Save"
+      : _props.type === "Review"
+      ? "Send"
+      : "Create";
   const weekDays: any = [
     "Sunday",
     "Monday",
@@ -94,17 +173,55 @@ export function HabitFormModal(_props: any) {
       console.log(error);
     }
   }
+  async function handleReviewSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const headers: any = {
+      Authorization: "Token " + localStorage.getItem("token"),
+    };
+    event.preventDefault();
+    const form: any = event.target;
+    const trackURL: string = "http://127.0.0.1:8000/track/";
+    const data: any = {
+      state: "D",
+      note: form.trackDescription.value,
+      effectiveness: form.trackEffectiveness.value,
+      habit: _props.url,
+    };
+    console.log(data);
+    try {
+      let response: any = await axios.post(trackURL, data, {
+        headers: headers,
+      });
+      console.log(response);
+      if (response.status) {
+        setUserSubmitInformation({
+          message: "Review created successfully!",
+          color: "#65902f",
+          success: true,
+        });
+        setTimeout(_props.reloadUS, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+      setUserSubmitInformation({
+        message: (error.response.data.name ?? [
+          "Select an icon to desribe your humor today! If you did and the error persist, try again later",
+        ])[0],
+        color: "#933",
+        success: false,
+      });
+    }
+  }
   async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const URL: string =
       _props.type === "Edit" ? _props.url : "http://127.0.0.1:8000/habit/";
     const form: HTMLFormElement = event.target as HTMLFormElement;
-    let cond1: boolean = form.EndHour.value < form.StartHour.value;
-    let cond2: boolean =
+    let habitEndsBeforeStartingByHour: boolean =
+      form.EndHour.value < form.StartHour.value;
+    let habitEndsBeforeStartingByMinutes: boolean =
       form.EndHour.value === form.StartHour.value &&
       form.StartMinutes.value >= form.EndMinutes.value;
-    console.log("cond1", cond1, "cond2", cond2);
-    if (cond1 || cond2) {
+    if (habitEndsBeforeStartingByHour || habitEndsBeforeStartingByMinutes) {
       setUserSubmitInformation({
         message: "Error! Start or End time values are incorrect",
         color: "#933",
@@ -131,9 +248,6 @@ export function HabitFormModal(_props: any) {
       form.EndHour.value,
       form.EndMinutes.value
     );
-    const headers: any = {
-      Authorization: "Token " + localStorage.getItem("token"),
-    };
     const data: any = {
       startTime: startTime,
       endTime: endTime,
@@ -144,10 +258,13 @@ export function HabitFormModal(_props: any) {
       data["user"] = localStorage.getItem("userURL");
     }
     try {
+      const headers: any = {
+        Authorization: "Token " + localStorage.getItem("token"),
+      };
+      console.log(data, headers);
       let response: any = await axios[
         _props.type === "Edit" ? "patch" : "post"
-      ](URL, data, { headers: headers }).catch();
-      console.log(response);
+      ](URL, data, { headers: headers });
       if (response.status === 201 || response.status === 200) {
         let msg: string =
           _props.type === "Edit"
@@ -200,6 +317,26 @@ export function HabitFormModal(_props: any) {
     }
     return gridItems;
   }
+  const DeleteButton: any = (props: any) => {
+    if (props.show) {
+      return (
+        <Button
+          variant="unstyled"
+          _focus={undefined}
+          type="submit"
+          form="habitForm"
+          disabled={userSubmitInformation.success}
+          color="#f30058"
+          p="1"
+          onClick={handleHabitDeletion}
+        >
+          Delete
+        </Button>
+      );
+    } else {
+      return <></>;
+    }
+  };
   return (
     <Modal
       isOpen={_props.isOpen}
@@ -219,8 +356,13 @@ export function HabitFormModal(_props: any) {
         w={{ base: "90%", md: "100%" }}
         p="2"
       >
-        <ModalHeader fontFamily="serif" rounded="md" bgColor={bgColor}>
-          {_props.type === "Edit" ? "Edit " + _props.name : "Habit"}
+        <ModalHeader
+          textTransform="capitalize"
+          fontFamily="serif"
+          rounded="md"
+          bgColor={bgColor}
+        >
+          {modalTitle}
         </ModalHeader>
         <ModalCloseButton _focus={undefined} />
         <ModalBody
@@ -230,30 +372,49 @@ export function HabitFormModal(_props: any) {
           alignItems="center"
           whiteSpace="pre-line"
         >
-          <form onSubmit={handleFormSubmit} name="habitForm" id="habitForm">
-            <Input
-              rounded="md"
-              required={true}
-              variant="flushed"
-              name="habitName"
-              placeholder="Name"
-              _placeholder={{ color: textColor }}
-            />
-            <Grid templateColumns="3" mt="1" justify="space-between">
-              <GridItem colSpan={3}>
-                {makeSelectionOf(weekDays, "Day", "")}
-              </GridItem>
-              {makeTimeInputGridItems(
-                makeSelectionOf,
-                { lists: [hours, minutes], labels: ["Hour", "Minutes"] },
-                "End"
-              )}
-            </Grid>
+          <form
+            onSubmit={
+              _props.type === "Review" ? handleReviewSubmit : handleFormSubmit
+            }
+            name="habitForm"
+            id="habitForm"
+          >
+            {_props.type !== "Review" && (
+              <>
+                <Input
+                  rounded="md"
+                  required={true}
+                  variant="flushed"
+                  name="habitName"
+                  placeholder="Name"
+                  _placeholder={{ color: textColor }}
+                />
+                <Grid templateColumns="3" mt="1" justify="space-between">
+                  <GridItem colSpan={3}>
+                    {makeSelectionOf(weekDays, "Day", "")}
+                  </GridItem>
+                  {makeTimeInputGridItems(
+                    makeSelectionOf,
+                    { lists: [hours, minutes], labels: ["Hour", "Minutes"] },
+                    "End"
+                  )}
+                </Grid>
+              </>
+            )}
+            {_props.type === "Review" && <EffectivenessIcons />}
             <Textarea
               mt="1"
               variant="outline"
-              name="habitDescription"
-              placeholder="Description"
+              name={
+                _props.type === "Review"
+                  ? "trackDescription"
+                  : "habitDescription"
+              }
+              placeholder={
+                _props.type === "Review"
+                  ? "Write a review note ..."
+                  : "Description"
+              }
               _placeholder={{ color: textColor }}
             />
           </form>
@@ -267,22 +428,7 @@ export function HabitFormModal(_props: any) {
             {userSubmitInformation.message}
           </Text>
           <Flex justify="space-evenly">
-            {_props.type === "Edit" && (
-              <>
-                <Button
-                  variant="unstyled"
-                  _focus={undefined}
-                  type="submit"
-                  form="habitForm"
-                  disabled={userSubmitInformation.success}
-                  color="red"
-                  p="1"
-                  onClick={handleHabitDeletion}
-                >
-                  Delete
-                </Button>
-              </>
-            )}
+            <DeleteButton show={_props.type === "Edit"} />
             <Button
               p="1"
               variant="unstyled"
@@ -291,7 +437,7 @@ export function HabitFormModal(_props: any) {
               form="habitForm"
               disabled={userSubmitInformation.success}
             >
-              {_props.type === "Edit" ? "Save" : "Create"}
+              {buttonText}
             </Button>
           </Flex>
         </ModalFooter>
